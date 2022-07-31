d89a39666572a86261a73a2b1c9a2ffb3c703638
DimaDemko <idmitriy4g@gmail.com>
2022-07-31 20:08:25 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
+const galleryDiv = document.querySelector(".gallery");
+const galleryHtml = addGalleryItemsHtml(galleryItems);
+galleryDiv.insertAdjacentHTML("beforeend", galleryHtml);
+galleryDiv.addEventListener("click", clickOnimageLink);
+
+function clickOnimageLink(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  basicLightbox
+    .create(
+      `
+    <img src=${event.target.dataset.source} width="800" height="600">
+`
+    )
+    .show();
+}
+
+function addGalleryItemsHtml(input) {
+  return input
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
